4baffc82e8ba9164aef92be5907b9bf8fce974c9
Ivan Borges <ivankunn@gmail.com>
2023-11-15 20:20:20 -0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+
+const botaoEstouComSorte = document.querySelector(`form [type="button"]`);
+
+botaoEstouComSorte.addEventListener("click", () => {
+
+    location.href = "https://class.hcode.com.br"
+
+})
